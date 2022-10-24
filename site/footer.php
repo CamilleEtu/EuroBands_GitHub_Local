e610be35dc7d2c004b3db3b1fc525ce29a7b93de
CamilleEtu <116064225+CamilleEtu@users.noreply.github.com>
2022-10-24 15:27:15 +0200

    Update footer.php

--- site/footer.php
+++ site/footer.php
@@ -2,17 +2,40 @@
 <html lang="fr">
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="css/style.css"/>
     </head>
 
     <body>
 
         <footer>
-            Plan du site avec navigation </br>
-            Mentions légales </br>
-            CGV </br>
-            Coordonnées et contact </br>
-            Liens réseaux sociaux </br>
-            A propos </br>
+            <div class="container">
+                <div class="RS">
+                    <a href=""><img src="images/facebook.png"></a>
+                    <a href=""><img src="images/insta.png"></a>
+                    <a href=""><img src="images/twitter.png"></a>
+                    <a href=""><img src="images/youtube.png"></a>
+                </div>
+                <nav>
+                    <ul>
+                        <a href=""><li>LOGO</li></a>
+                        <li>Plan du site
+                            <ul>
+                                <a href=""><li>A propos de EuroBands</li></a>
+                                <a href=""><li>Réservation</li></a>
+                            </ul>
+                        </li>
+                        <a href=""><li>Mentions légales</li></a>
+                        <a href=""><li>CGV</li></a>
+                        <li>Coordonnées
+                            <p>Adresse</p>
+                            <p>Code postal</p>
+                            <p>Mail</p>
+                            <p>Téléphone</p>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            
         </footer>
     </body>
 </html>
