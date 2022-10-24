8afc43af3889812d25eadc220052d403ceb5cc7b
Valentin Lafon <59995009+HekisaOF@users.noreply.github.com>
2022-10-24 17:38:18 +0200

    Revert "Merge branch 'main' of https://github.com/CamilleEtu/EuroBands_Online"
    This reverts commit eb5df652e6f771d78827b803c38646327dbd899e, reversing
    changes made to 1cd7876dfcbcd2ace68475a62923950d1507d4cb.

--- site/header.php
+++ site/header.php
@@ -1,25 +1,22 @@
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
-        <meta type="descritpion" content="eurobands, festival, music, international">
-        <meta charset="utf-8">
-        <meta type="keywords" content="eurobands, festival, music, international">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
-        <link rel="stylesheet" type="text/css" href="css/styleheader.css"/>
-    </head>
-
-    <body>
-        <header>
-            <div class="container">
-                <a href=""><img class="logo" src="img/logo_b.png"></a>
-                <nav class="navbar">
-                    <ul>
-                        <li><a href="">Artiste</a></li>
-                        <li><a href="">En savoir plus</a></li>
-                    </ul>
-                </nav>
-                <a href="">Réservation</a>
-            </div>
-        </header>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./css/style_header.css" rel="stylesheet">
+    <title>Header</title>
+</head>
+<body>
+    <div class="container">
+        <nav class="navbar">
+            <ul>
+                <li><a href=""><img class="logo_nav" src="./img/logo_n.png"></a></li>
+                <li><a href="">Artiste</a></li>
+                <li><a href="">En savoir plus</a></li>
+                <li><a href="">Réservation</a></li>
+            </ul>
+        </nav>
+    </div>
+</body>
 </html>
