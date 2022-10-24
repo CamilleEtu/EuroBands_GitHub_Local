f274136c2d1a04f24abc32d2a5253ea828079965
Valentin Lafon <59995009+HekisaOF@users.noreply.github.com>
2022-10-24 14:22:13 +0200

    Création fichiers Header.php, Index.php et début d'edition

--- site/header.php
+++ site/header.php
@@ -1,1 +1,19 @@
-
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+</head>
+<body>
+    <div class="container">
+        <nav>
+            <ul>
+                <li><a>Artiste</a></li>
+                <li><a>En savoir plus</a></li>
+            </ul>
+        </nav>
+    </div>
+</body>
+</html>
