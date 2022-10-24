025adeaa9079b608bc70c3c8caf53ca3872de998
CamilleEtu <116064225+CamilleEtu@users.noreply.github.com>
2022-10-24 14:09:57 +0200

    Update footer.php

--- site/footer.php
+++ site/footer.php
@@ -1,1 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+    </head>
 
+    <body>
+
+        <footer>
+            Plan du site avec navigation </br>
+            Mentions légales </br>
+            CGV </br>
+            Coordonnées et contact </br>
+            Liens réseaux sociaux </br>
+            A propos </br>
+        </footer>
+    </body>
+</html>
