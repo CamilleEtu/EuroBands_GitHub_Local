534dcc8f436dd8fd0f592e785e5871dc465980dd
Valentin Lafon <59995009+HekisaOF@users.noreply.github.com>
2022-10-24 16:56:56 +0200

    Update header.php

--- site/header.php
+++ site/header.php
@@ -4,14 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./css/style_header.css" rel="stylesheet">
     <title>Header</title>
 </head>
 <body>
     <div class="container">
-        <nav>
+        <nav class="navbar">
             <ul>
-                <li><a>Artiste</a></li>
-                <li><a>En savoir plus</a></li>
+                <li><a href=""><img class="logo_nav" src="./img/logo_n.png"></a></li>
+                <li><a href="">Artiste</a></li>
+                <li><a href="">En savoir plus</a></li>
+                <li><a href="">Réservation</a></li>
             </ul>
         </nav>
     </div>
