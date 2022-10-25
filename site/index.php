da53ae4dce91fd19582cb2b2dd0848dc6d86f493
Valentin Lafon <59995009+HekisaOF@users.noreply.github.com>
2022-10-25 11:50:36 +0200

    Moddification Index
    Création la section Artistes

--- site/index.php
+++ site/index.php
@@ -23,14 +23,79 @@
         </header>
 
             <div class="container_p1">
-                <h1 class="titre_accueil">TITRE EXPLICATION DU FESTIVAL</h1>
+                <h1 class="titre_accueil">BIENVENUE A L'EUROBANDS</h1>
                 <div>
-                    <img src="./img/spaghettis.jpg">
+                    <img src="./img/Angele.png">
                     <div>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi placerat ligula eu commodo consequat. Proin convallis accumsan libero, vitae sagittis ligula dictum pellentesque. Nam at lorem nec odio consectetur cursus. Proin eu pharetra ipsum, bibendum dapibus urna. Proin tempus massa bibendum mauris consectetur, eu viverra nibh pellentesque.</p>
                         <a href="#">En savoir plus</a>
                     </div>
                 </div>
+            </div>
+
+            <div class="container_p2">
+                <h1 class="titre_accueil">ARTISTES</h1>
+                <ul class="image-gallery">
+                    <li>
+                        <a href="#"><img src="./img/SAM_the_kid.png" alt=""/></a>
+                        <div class="overlay"><span>Sam The Kid</span></div>
+                    </li>
+
+                    <li>
+                    <a href="#"><img src="./img/Angele.png" alt="" />
+                        <div class="overlay"><span>Angèle</span></div>
+                    </li>
+
+                    <li>
+                    <a href="#"><img src="./img/ARMIN_VAN_BUUREN.png" alt="" /></a>
+                        <div class="overlay"><span>Armine Van Buuren</span></div>
+                    </li>
+
+                    <li>
+                    <a href="#"><img src="./img/BigfloEtOli.png" alt="" /></a>
+                        <div class="overlay"><span>Bigflo et Oli</span></div>
+                    </li>
+
+                    <li>
+                    <a href="#"><img src="./img/Enrique.png" alt="" /></a>
+                        <div class="overlay"><span>Enrique</span></div>
+                    </li>
+
+                    <li>
+                    <a href="#"><img src="./img/HLADNO_PIVO.png" alt="" /></a>
+                        <div class="overlay"><span>Hladno Pivo</span></div>
+                    </li>
+
+                    <li>
+                    <a href="#"><img src="./img/Julien.png" alt="" /></a>
+                        <div class="overlay"><span>Julien Dorée</span></div>
+                    </li>
+
+                    <li>
+                    <a href="#"><img src="./img/MERO.png" alt="" /></a>
+                        <div class="overlay"><span>Mero</span></div>
+                    </li>
+
+                    <li>
+                    <a href="#"><img src="./img/Pegboard_Nerds.png" alt="" /></a>
+                        <div class="overlay"><span>Pegboard Nerds</span></div>
+                    </li>
+
+                    <li>
+                    <a href="#"><img src="./img/The_Knife.png" alt="" /></a>
+                        <div class="overlay"><span>The Knife</span></div>
+                    </li>
+
+                    <li>
+                    <a href="#"><img src="./img/THE_RASMUS.png" alt="" /></a>
+                        <div class="overlay"><span>The Rasmus</span></div>
+                    </li>
+
+                    <li>
+                    <a href="#"><img src="./img/U2.png" alt="" /></a>
+                        <div class="overlay"><span>U2</span></div>
+                    </li>
+                </ul>
             </div>
 
 
