32a7f4ea30f1b5a686a69da6de9e43431303fcab
CamilleEtu <116064225+CamilleEtu@users.noreply.github.com>
2022-10-25 14:06:32 +0200

    Add files via upload

---
+++ site/carrousel_V2.php
@@ -0,0 +1,7 @@
+<script src="js/carrousel_V2.js"></script>
+
+<section class="image">
+    <button class="fleche" id="g"><img src="images/fleche.png" class="bg"></button>
+    <img id="af" src="images/image5.jpg">
+    <button class="fleche" id="d"><img src="images/fleche.png"></button>
+</section>
