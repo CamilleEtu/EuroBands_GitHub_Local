88c1e68f7e6823da85791b4d9f84653a5196b5a1
Valentin Lafon <59995009+HekisaOF@users.noreply.github.com>
2022-10-25 16:28:54 +0200

    Creation CSS pour la page Réservation et ajout de valeurs

--- site/reservation.php
+++ site/reservation.php
@@ -4,10 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" src="./css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="css/styleReservation.css"/>
     <title>EuroBands - Réservation</title>
 </head>
 <body>
-    
+    <header>
+        <?php
+        include './header.php';
+        ?>
+    </header>
+    <div class="container_reserv">
+                <h1 class="titre_accueil">Réservation gratuite</h1>
+                <div>
+                    <div>
+                        <h2>Nombre de place</h2>
+                    </div>
+                    <div class="resume">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi placerat ligula eu commodo consequat. Proin convallis accumsan libero, vitae sagittis ligula dictum pellentesque. Nam at lorem nec odio consectetur cursus. Proin eu pharetra ipsum, bibendum dapibus urna. Proin tempus massa bibendum mauris consectetur, eu viverra nibh pellentesque.</p>
+                        <p>Nam maximus libero a posuere malesuada. Donec sollicitudin, augue vitae lacinia rhoncus, risus sem tempus odio, vitae pulvinar augue nibh id dui. Proin eget purus vitae ex consequat bibendum vel a est.</p>
+                    </div>
+                </div>
+            </div>
+    <footer>
+        <?php
+        include './footer.php';
+        ?>
+    </footer>
 </body>
 </html>
