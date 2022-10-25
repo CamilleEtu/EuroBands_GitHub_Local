437950c5305f6f85a0a343ee6e4e7b0d1b2a0d3a
Valentin Lafon <59995009+HekisaOF@users.noreply.github.com>
2022-10-25 09:27:33 +0200

    Correction HTML Index

--- site/index.php
+++ site/index.php
@@ -1,29 +1,72 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link href="./css/style.css" rel="stylesheet"> -->
-    <title>EuroBands</title>
-</head>
-<body>
-    <header>
-    <?php include 'header.php'; ?>
-    </header>
-        
+    <head>
+        <meta type="descritpion" content="eurobands, festival, music, international">
+        <meta charset="utf-8">
+        <meta type="keywords" content="eurobands, festival, music, international">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
+        <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    </head>
 
-    <!-- <div class="container_p1">
-        <h1>TITRE EXPLICATION DU FESTIVAL</h1>
-        <div>
-            <img src="/img/spaghettis.jpg" alt="Image d'illustration">
-            <p>uiatgzyhtzr^hgiytgrbrzrthzrthzjyjyzjyjrzhhjkejfjgizhekflojhofoejheidieudjfuidehfiufiupzfzhiuhgriuahzrygueihtuhtehrrzijghruzhgurphgghzihrgugzhrgpiuhz</p>
-            <a href="COUCOU">Voir plus</a>
-        </div>
-    </div> -->
+    <body>
+        <header>
+            <div class="container_header">
+                <a href=""><img class="logo" src="img/logo_b.png"></a>
+                <nav class="navbar">
+                    <ul>
+                        <li><a href="">Artiste</a></li>
+                        <li><a href="">En savoir plus</a></li>
+                    </ul>
+                </nav>
+                <a class="resa" href="">Réservation</a>
+            </div>
+        </header>
 
-    <footer>
-        <?php include 'footer.php'; ?>
-    </footer>
-</body>
+        <main>
+            <div class="container_p1">
+                <h1>TITRE EXPLICATION DU FESTIVAL</h1>
+                <div>
+                    <img src="/img/spaghettis.jpg" alt="Image d'illustration">
+                    <p>uiatgzyhtzr^hgiytgrbrzrthzrthzjyjyzjyjrzhhjkejfjgizhekflojhofoejheidieudjfuidehfiufiupzfzhiuhgriuahzrygueihtuhtehrrzijghruzhgurphgghzihrgugzhrgpiuhz</p>
+                    <a href="COUCOU">Voir plus</a>
+                </div>
+            </div>
+        </main>
+
+
+        <footer>
+            <div class="container_footer">
+                <div class="RS">
+                    <a href=""><img src="img/facebook.png"></a>
+                    <a href=""><img src="img/insta.png"></a>
+                    <a href=""><img src="img/twitter.png"></a>
+                    <a href=""><img src="img/youtube.png"></a>
+                </div>
+                <nav>
+                    <ul>
+                        <a href=""><li><img class="logo" src="img/logo_b.png"></li></a>
+                        <li><h3>Plan du site</h3>
+                            <ul>
+                                <a href=""><li>A propos de EuroBands</li></a>
+                                <a href=""><li>Réservation</li></a>
+                            </ul>
+                        </li>
+                        <li><h3>Partie légale</h3>
+                            <ul>
+                                <a href=""><li>Mentions légales</li></a>
+                                <a href=""><li>CGV</li></a>
+                            </ul>
+                        </li>
+                        <li><h3>Coordonnées</h3>
+                            <p>&emsp;Place du Breuil</p>
+                            <p>&emsp;43 000 Le Puy En Velay</p>
+                            <p>&emsp;eurobands@mail.com</p>
+                            <p>&emsp;01 02 03 04 05</p>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            
+        </footer>
+    </body>
 </html>
