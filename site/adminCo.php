6dceb31aa1c67dbd6d1bb13ae40f29bf6fa140f9
quentinGlayat <quentin.glayat@etu.uca.fr>
2022-10-25 09:31:20 +0200

    Create adminCo.php

---
+++ site/adminCo.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./css/style.css" rel="stylesheet">
+    <title>EuroBands Admin</title>
+</head>
+
+<?php
+
+if (isset($_POST["login"])) {
+    if ($_POST["login"] == "admin" && $_POST["mdp"] == "eurobandsMDP") {
+        $_SESSION['admin'] = 'true';
+        header("location: index.php");
+        exit();
+    }
+    else{
+        if ($_POST["login"] != "admin" && $_POST["mdp"] != "eurobandsMDP") {
+            echo "<h1>Votre login et votre mot de passe sont incorects !</h1>";
+        }
+        else{
+            if ($_POST["login"] != "admin") {
+                echo "<h1>Votre login est incorrect !</h1>";
+            }
+            else{
+                echo "<h1>Votre mot de passe est incorrect !</h1>";
+            }
+        }
+    }
+}
+
+?>
+<body>
+
+    <div class="continer">
+        <form action="" method="POST">
+            <label for="login">Entrez votre login</label>
+            <input type="text" name="login" id="login" required>
+            
+            <label for="login">Entrez votre mot de passe</label>
+            <input type="password" name="mdp" id="mdp" required>
+
+            <input type="submit" value="Connexion">
+    </div>
+
+</body>
+</html>
