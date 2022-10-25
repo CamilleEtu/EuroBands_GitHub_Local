44f7b814561acb3abd68da8056dad6bca1e82eb1
Valentin Lafon <59995009+HekisaOF@users.noreply.github.com>
2022-10-25 15:37:44 +0200

    MAJ page Information

--- site/page_information.php
+++ site/page_information.php
@@ -26,6 +26,33 @@
                     </div>
                 </div>
             </div>
+
+            <div class="container_info">
+                <div>
+                    <div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi placerat ligula eu commodo consequat. Proin convallis accumsan libero, vitae sagittis ligula dictum pellentesque. Nam at lorem nec odio consectetur cursus. Proin eu pharetra ipsum, bibendum dapibus urna. Proin tempus massa bibendum mauris consectetur, eu viverra nibh pellentesque.</p>
+                        <p>Nam maximus libero a posuere malesuada. Donec sollicitudin, augue vitae lacinia rhoncus, risus sem tempus odio, vitae pulvinar augue nibh id dui. Proin eget purus vitae ex consequat bibendum vel a est.</p>
+                    </div>
+                    <img src="./img/Angele.png">
+                </div>
+            </div>
+            
+            <div class="container_info">
+                <div>
+                    <img src="./img/Angele.png">
+                    <div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi placerat ligula eu commodo consequat. Proin convallis accumsan libero, vitae sagittis ligula dictum pellentesque. Nam at lorem nec odio consectetur cursus. Proin eu pharetra ipsum, bibendum dapibus urna. Proin tempus massa bibendum mauris consectetur, eu viverra nibh pellentesque.</p>
+                        <p>Nam maximus libero a posuere malesuada. Donec sollicitudin, augue vitae lacinia rhoncus, risus sem tempus odio, vitae pulvinar augue nibh id dui. Proin eget purus vitae ex consequat bibendum vel a est.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container_info">
+            <h2 class="titre_accueil">Vidéo de présentation de la technologie hologramme</h1>
+                    <div class="video_1">
+                    <iframe width="853" height="480" src="https://www.youtube.com/embed/71vdr1vUy08" title="Principe de fonctionnement des hologrammes." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+            </div>
                 
         <footer>
             <?php
