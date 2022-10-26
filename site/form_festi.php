ea0b71bec7de1afdda97dc6f1d5b97d3497fd2a2
CamilleEtu <116064225+CamilleEtu@users.noreply.github.com>
2022-10-26 11:43:27 +0200

    Update form_festi.php

--- site/form_festi.php
+++ site/form_festi.php
@@ -1,30 +1,115 @@
+<?php
+
+    //var_dump($_POST);
+    
+    $bdd = new PDO('mysql:host=localhost;port=3306;dbname=eurobands','root', '');
+        // récupère toutes les informations de la table "festivalier"
+        $requete = 'SELECT * FROM festivalier';
+        $resultats = $bdd -> query($requete) ;
+        // met les résultats sous forme de tableau
+        $tabFesti=$resultats -> fetchAll() ;
+        $resultats -> closeCursor() ;
+        // compte les données du tableau
+        $nbFesti = count($tabFesti) ;
+
+        // récupère toutes les informations de la table "jour"
+        $requete = 'SELECT * FROM jour';
+        $resultats = $bdd -> query($requete) ;
+        // met les résultats sous forme de tableau
+        $tabJour=$resultats -> fetchAll() ;
+        $resultats -> closeCursor() ;
+        // compte les données du tableau
+        $nbJour = count($tabJour) ;
+
+        if (isset($_GET["ajout"])){
+            if (isset($_POST['nom'])){
+                $nom = $_POST['nom'];
+                $prenom = $_POST['prenom'];
+                $mail = $_POST['mail'];
+                // requête permet d'enregitrer les données saisies par l'utilisateur dans la base de données
+                $reqpreparee = $bdd -> prepare("INSERT INTO festivalier (nom_festivalier, prenom_festivalier, mail_festivalier) VALUES(:nom,:prenom,:mail)");
+                $reqpreparee -> bindParam(':nom', $nom, PDO::PARAM_STR);
+                $reqpreparee -> bindParam(':prenom', $prenom, PDO::PARAM_STR);  
+                $reqpreparee -> bindParam(':mail', $mail, PDO::PARAM_STR);
+       
+
+                // Appel de la procédure stockée
+                $succes = $reqpreparee->execute();
+                $id_festivalier = $bdd->lastInsertId();
+                if($succes==true){
+                    echo 'Vous êtes maintenant pré-inscri !</br>';
+                }
+                else { 
+                    echo 'Erreur de connexion à la base de données :( </br>';        
+                }
+                // compte les données du tableau
+                $nbJourReserve = count( $_POST["id_jour"]) ;
+
+                $reqpreparee = $bdd -> prepare('INSERT INTO reserve(id_festivalier,id_jour) VALUES (:id_festivalier,:id_jour)');
+                for ($i=0;$i<$nbJourReserve;$i++){
+                    $reqpreparee->execute(array(':id_festivalier' => $id_festivalier,':id_jour' => $_POST["id_jour"][$i]));
+                }
+
+
+            }
+
+        }
+
+        /*
+        1) on insert un festivalier
+
+        */
+?>
+
 <html>
-    <form action="Festivalier.php" method="POST" class="form_festi">
-        <div>
-            <label for="nom">Entrez votre NOM : </label>
-            <input type="text" name="nom" id="name" required>
-        </div>
-        <div>
-            <label for="prenom">Entrez votre Prénom : </label>
-            <input type="text" name="prenom" id="surname" required>
-        </div>
-        <div>
-            <label for="mail">Entrez votre adresse email : </label>
-            <input type="email" name="mail" id="email" required>
-        </div>
-        <div>
-            Vous souhaitez vous inscrire pour :
-            <input type="checkbox" id="1j" name="1j">
-            <label for="scales">1 jour</label>
-            <input type="checkbox" id="2j" name="2j">
-            <label for="scales">2 jours</label>
-            <input type="checkbox" id="3j" name="3j">
-            <label for="scales">3 jours</label>
-            <input type="checkbox" id="Tj" name="Tj">
-            <label for="scales">Les 4 jours</label>
-        </div>
-        <div>
-            <input type="submit" value="Se pré-inscrire">
-        </div>
+    <?php
+        if (!isset($_GET["ajout"])){
+    ?>
+
+    <h1>Formulaire de pré-inscription</h1>
+    <form action="form_festi.php?ajout=1" method="POST" class="form_festi">
+        <fieldset>
+            <div>
+                <label for="nom">Entrez votre NOM : </label>
+                <input type="text" name="nom" id="nom" placeholder="NOM" required>
+            </div>
+            <div>
+                <label for="prenom">Entrez votre Prénom : </label>
+                <input type="text" name="prenom" id="prenom" placeholder="Prénom" required>
+            </div>
+            <div>
+                <label for="mail">Entrez votre adresse email : </label>
+                <input type="email" name="mail" id="mail" placeholder="email" required>
+            </div>
+           
+            <div>
+                Vous souhaitez vous pré-inscrire pour :</br>
+                <?php
+                    
+                    for ($i=0;$i<$nbJour;$i++){
+                        $date = date ('l', strtotime ($tabJour[$i]['date']));
+                        
+                        // utilise les données récupérées grâce à la requête pour proposer les différents jours enregistrés dans la base de donnée
+                        echo '<input type=checkbox name="id_jour[]" id="jour'.$tabJour[$i]['id_jour'].'" value="'.$tabJour[$i]['id_jour'].'"/><label for="jour'.$tabJour[$i]['id_jour'].'">'.$date.'</label></br>';
+                    }
+                ?>
+            </div>
+            <div>
+                <input type="submit" value="Se pré-inscrire">
+                <input type="reset" value="Effacer">
+            </div>
+        </fieldset>
     </form>
+
+    <?php
+        }
+    ?>
+
 </html>
+
+<?php
+if (isset($_POST['nom'])){
+
+        echo '<a href = "../index.php"><button class="acc">Accueil</a></button>';
+    }
+?>
