64e2c8f2983a2836155e940dfa110ddef3d87fc1
Valentin Lafon <59995009+HekisaOF@users.noreply.github.com>
2022-10-27 15:36:02 +0200

    Modif carrousel image

---
+++ site/carrousel.php
@@ -0,0 +1,22 @@
+<!-- Swiper -->
+<div class="swiper mySwiper">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide"><img src="./img/GLOBAL txt FR.png"></div>
+                <div class="swiper-slide"><img src="./img/ELECTRO POP txt FR.png"></div>
+                <div class="swiper-slide"><img src="./img/ELECTRO RAP txt FR.png"></div>
+                <div class="swiper-slide"><img src="./img/POP ROCK txt FR.png"></div>
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+        <!-- Initialize Swiper -->
+        <script type="module">
+            var swiper = new Swiper(".swiper", {
+                spaceBetween: 30,
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: true,
+                },
+            });
+        </script>
--- site/index.php
+++ site/index.php
@@ -36,33 +36,9 @@ $nbArt = count($tableauArt);
     ?>
     <main>
 
-        <!-- Swiper -->
-        <div class="swiper mySwiper">
-            <div class="swiper-wrapper">
-                <div class="swiper-slide"><img src="./img/GLOBAL.png"></div>
-                <div class="swiper-slide">Slide 2</div>
-                <div class="swiper-slide">Slide 3</div>
-                <div class="swiper-slide">Slide 4</div>
-                <div class="swiper-slide">Slide 5</div>
-                <div class="swiper-slide">Slide 6</div>
-                <div class="swiper-slide">Slide 7</div>
-                <div class="swiper-slide">Slide 8</div>
-                <div class="swiper-slide">Slide 9</div>
-            </div>
-            <div class="swiper-pagination"></div>
-        </div>
-
-        <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
-        <!-- Initialize Swiper -->
-        <script type="module">
-            var swiper = new Swiper(".swiper", {
-                spaceBetween: 30,
-                pagination: {
-                    el: ".swiper-pagination",
-                    clickable: true,
-                },
-            });
-        </script>
+        <?php
+        include "./carrousel.php";
+        ?>
 
 
 
