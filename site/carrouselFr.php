c552347ab96a6bda54b663a7c9c57a9989466b64
Valentin Lafon <59995009+HekisaOF@users.noreply.github.com>
2022-10-28 11:44:38 +0200

    Slider autoplay

--- site/carrouselFr.php
+++ site/carrouselFr.php
@@ -1,22 +1,25 @@
 <!-- Swiper -->
 <div class="swiper mySwiper">
-            <div class="swiper-wrapper">
-                <div class="swiper-slide"><img src="./img/ELECTRO RAP txt FR.png"></div>
-                <div class="swiper-slide"><img src="./img/ELECTRO POP txt FR.png"></div>
-                <div class="swiper-slide"><img src="./img/POP ROCK txt FR.png"></div>
-                <div class="swiper-slide"><img src="./img/GLOBAL txt FR.png"></div>
-            </div>
-            <div class="swiper-pagination"></div>
-        </div>
+    <div class="swiper-wrapper">
+        <div class="swiper-slide"><img src="./img/ELECTRO RAP txt FR.png"></div>
+        <div class="swiper-slide"><img src="./img/ELECTRO POP txt FR.png"></div>
+        <div class="swiper-slide"><img src="./img/POP ROCK txt FR.png"></div>
+        <div class="swiper-slide"><img src="./img/GLOBAL txt FR.png"></div>
+    </div>
+    <div class="swiper-pagination"></div>
+</div>
 
-        <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
-        <!-- Initialize Swiper -->
-        <script type="module">
-            var swiper = new Swiper(".swiper", {
-                spaceBetween: 30,
-                pagination: {
-                    el: ".swiper-pagination",
-                    clickable: true,
-                },
-            });
-        </script>
+<script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+<!-- Initialize Swiper -->
+<script type="module">
+    var swiper = new Swiper(".swiper", {
+        spaceBetween: 30,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        autoplay: {
+            delay: 4000,
+        },
+    });
+</script>
