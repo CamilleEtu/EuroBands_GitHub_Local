a29851ee7e24920f47404b1c0388371027a4bb37
quentinGlayat <quentin.glayat@etu.uca.fr>
2022-11-08 15:14:06 +0100

    changement petite erreur de compréhension du texte

--- site/lang/lang-en.php
+++ site/lang/lang-en.php
@@ -11,7 +11,7 @@ define('index_car', "carrouselEn.php");
 
 define('index_titre2', "Artists");
 
-define('index_p1', 'In 2023, we celebrate the 30th anniversary of the creation of the European Union. On this occasion, Le Puy en Velay has chosen to organize an exceptional event, equal to the occasion. 4 evenings, 12 artists and groups from all over Europe. Come celebrate this unique event from <strong>June 22 to 25, 2023, Place du Breuil in Puy en Velay</strong>. To celebrate the interculturality of the event, the concerts will be broadcasted on television, and in some European countries, it will be possible to attend in holographic version.</br> ');
+define('index_p1', 'In 2023, we celebrate the 30th anniversary of the creation of the European Union. On this occasion, Le Puy en Velay has chosen to organize an exceptional event to celebrate this anniversary. 4 evenings, 12 artists and groups from all over Europe. Come celebrate this unique event from <strong>June 22 to 25, 2023, Place du Breuil in Puy en Velay</strong>. To celebrate the interculturality of the event, the concerts will be broadcasted on television, and in some European countries, it will be possible to attend in holographic version.</br> ');
 
 define("index_more", "See More");
 
