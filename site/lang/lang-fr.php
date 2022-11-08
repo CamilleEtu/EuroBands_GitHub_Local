7ab2ec1d6dc4981d8c47635ebe805e1e01d320be
CamilleEtu <116064225+CamilleEtu@users.noreply.github.com>
2022-11-08 14:23:51 +0100

    Update lang-fr.php

--- site/lang/lang-fr.php
+++ site/lang/lang-fr.php
@@ -12,7 +12,7 @@ define('index_car', "carrouselFr.php");
 define('index_titre2', "Artistes");
 
 define('index_p1', 'En 2023, nous célébrons les 30 ans de la création de l’Union Européenne. A cette occasion, Le Puy en Velay a choisi d’organiser un événement exceptionnel, à la hauteur de l\'événement. 4 soirées, 12 artistes et groupes venus des 4 coins de l’Europe.. 
-Venez célébrer cet événement unique du 22 au 25 juin 2023, place du Breuil au Puy en Velay. Pour célébrer l\'interculturalité de l’événement, les concerts seront retransmis  à la télévision, et dans différents pays d’Europe, il sera possible d’y assister en version holographique.</br> ');
+Venez célébrer cet événement unique du <strong>22 au 25 juin 2023, place du Breuil au Puy en Velay</strong>. Pour célébrer l\'interculturalité de l’événement, les concerts seront retransmis  à la télévision, et dans différents pays d’Europe, il sera possible d’y assister en version holographique.</br> ');
 
 define("index_more", "Voir Plus");
 
